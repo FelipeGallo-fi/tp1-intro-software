0eb3905ea05d74dcefb53d8071d3ae6d3b522e41
FelipeGallo05 <gallofelipe6@gmail.com>
2024-09-25 16:47:03 -0300

    Trabaje en los 3 mejores tiempos

--- acertijo3/acertijo3.sh
+++ acertijo3/acertijo3.sh
@@ -6,15 +6,27 @@ readonly REGEX_ANIOS='[0-9]{4}'
 segundos_ordenados=$(grep -oE "$REGEX_SEGUNDOS" "$1" | sort -n |uniq );
 anios_ordenados=$(grep -oE "$REGEX_ANIOS" "$1" | sort -n | uniq)
 
-resultados=""
+resultados="";
+
 for anio in $anios_ordenados;do
+
     for seg in $segundos_ordenados; do
+
     REGEX_ANIOS_SEGS="$anio,$seg\$"
-    anios_por_segundos=$(grep -E "$REGEX_ANIOS_SEGS" "$1" | grep -v '^$' );
+
+    anios_por_segundos=$(grep -E "$REGEX_ANIOS_SEGS" "$1" );
+
     if [[ ! -z "$anios_por_segundos" ]]; then
             resultados+="$anios_por_segundos"$'\n'
     fi
     done
+
 done
 
-echo "$resultados" | uniq;
+# echo "$resultados" ;
+
+tres_mejores_tiempos=$(grep -E "$REGEX_ANIOS" "$resultados" | sort -n | head -n 3);
+
+echo "$tres_mejores_tiempos" > acertijo3.txt
+
+cat acertijo3.txt;
