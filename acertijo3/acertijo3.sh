6d2940abc364612cc5f40e84bcbd989c35ad46a8
FelipeGallo05 <gallofelipe6@gmail.com>
2024-10-02 19:37:32 -0300

    acertijo 6

--- acertijo3/acertijo3.sh
+++ acertijo3/acertijo3.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-readonly REGEX_SEGUNDOS='[0-9]+$';
+readonly REGEX_MINUTOS='[0-9]+$';
 readonly REGEX_ANIOS='[0-9]{4}'
 readonly archivo="$1"
-readonly segundos_ordenados=$(grep -oE "$REGEX_SEGUNDOS" "$1" | sort -n |uniq );
+readonly minutos_ordenados=$(grep -oE "$REGEX_MINUTOS" "$1" | sort -n |uniq );
 readonly anios_ordenados=$(grep -oE "$REGEX_ANIOS" "$1" | sort -n | uniq)
 
 ordenar_anios(){
     local lineas_ordenadas="";
     local anio;
-    local seg;
+    local mins;
     for anio in $1;do
 
-        for seg in $2; do
+        for mins in $2; do
 
-            REGEX_ANIOS_SEGS="$anio,$seg\$"
+            REGEX_ANIOS_MINS="$anio,$mins\$"
 
-            anios_por_segundos=$(grep -E "$REGEX_ANIOS_SEGS" "$archivo" );
+            anios_por_MINUTOS=$(grep -E "$REGEX_ANIOS_MINS" "$archivo" );
 
-            if [[ ! -z "$anios_por_segundos" ]]; then
-            lineas_ordenadas+="$anios_por_segundos"$'\n'
+            if [[ ! -z "$anios_por_minutos" ]]; then
+            lineas_ordenadas+="$anios_por_minutos"$'\n'
             fi
         done
 
@@ -29,13 +29,13 @@ ordenar_anios(){
 }
 
 tres_mejores_tiempos_generales(){
-    mejores_tiempos=$(echo "$1" | grep -oE "$REGEX_SEGUNDOS" | sort -n | head -n 3);
+    mejores_tiempos=$(echo "$1" | grep -oE "$REGEX_MINUTOS" | sort -n | head -n 3);
 
     local tres_mejores="";
 
-    for seg in $mejores_tiempos;do
+    for mins in $mejores_tiempos;do
 
-        lineas_mejores_tiempos=$(echo "$1" | grep -E "\,$seg\$");
+        lineas_mejores_tiempos=$(echo "$1" | grep -E "\,$mins\$");
         tres_mejores+="$lineas_mejores_tiempos"$'\n'
     done
 
@@ -47,15 +47,15 @@ infractores(){
     local auxiliar="";
     for anio in $2; do
         lineas_anio=$(echo "$1" | grep "/$anio,");
-        mejores_tiempos=$(echo "$lineas_anio" | grep -oE "$REGEX_SEGUNDOS" | sort -n | head -n 3);
-        for seg in $mejores_tiempos;do
-            mejores_anios_por_segundo=$(echo "$lineas_anio" | grep -E ",$seg\$");
-            auxiliar+="$mejores_anios_por_segundo"$'\n'
+        mejores_tiempos=$(echo "$lineas_anio" | grep -oE "$REGEX_MINUTOS" | sort -n | head -n 3);
+        for mins in $mejores_tiempos;do
+            mejores_tiempos_por_anio=$(echo "$lineas_anio" | grep -E ",$mins\$");
+            auxiliar+="$mejores_tiempos_por_anio"$'\n'
         done
     done
     echo "$auxiliar" | grep -v '^$' > $3;
 }
 
-lineas_ordenadas=$(ordenar_anios "$anios_ordenados" "$segundos_ordenados");
+lineas_ordenadas=$(ordenar_anios "$anios_ordenados" "$minutos_ordenados");
 tres_mejores_tiempos_generales "$lineas_ordenadas";
 infractores "$lineas_ordenadas" "$anios_ordenados" infractores.txt;
